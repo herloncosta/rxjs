3cd7e5a8cfc8657b4b292b98f519d4bbda10f41d
herloncosta <herlon36@gmail.com>
2022-07-11 19:21:21 -0300

    promise 01

--- funcoes/promises01.js
+++ funcoes/promises01.js
@@ -1,13 +1,24 @@
+// setTimeout(function () {
+//     console.log("Executando callback...");
+
+//     setTimeout(function () {
+//         console.log("Executando callback...");
+
+//         setTimeout(function () {
+//             console.log("Executando callback...");
+//         }, 2000);
+//     }, 2000);
+// }, 2000);
+
 function esperarPor(tempo = 2000) {
-	return new Promise(function (resolve) {
-		setTimeout(function () {
-			console.log("Executando promise...");
-			resolve("Resolvido...");
-		}, tempo);
-	});
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.log("Executando callback...");
+            resolve("Resolvido.");
+        }, tempo);
+    });
 }
 
-esperarPor(3000)
-	.then(() => esperarPor())
-	.then(() => esperarPor())
-	.then(console.log);
+esperarPor(3000) // espera 3 segundos
+    .then(() => esperarPor(3000)) // espera mais 3 segundos
+    .then(() => esperarPor()); // espera 2 segundos
