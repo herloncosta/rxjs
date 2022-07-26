c3cd461e6f4bc2c3101aeed43c0a874cc4dbb6b8
herloncosta <herlon36@gmail.com>
2022-07-25 22:36:57 -0300

    refactoring

--- projeto01/src/index.js
+++ projeto01/src/index.js
@@ -4,8 +4,5 @@ const path = require("path");
 const caminho = path.join(__dirname, "legendas");
 
 fn.lerDiretorio(caminho)
-    .then((caminhos) => {
-        const resultado = fn.filtrarExtensao(caminhos, "srt");
-        return resultado;
-    })
+    .then((caminhos) => fn.filtrarExtensao(caminhos, "srt"))
     .then(console.log);
