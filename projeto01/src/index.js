68ebdf851528d568cb4a22c51e7affc8bd9b4ff5
herloncosta <herlon36@gmail.com>
2022-07-30 08:01:11 -0300

    revisao

--- projeto01/src/index.js
+++ projeto01/src/index.js
@@ -21,13 +21,19 @@ const simbolos = [
     "'",
 ];
 
+const mesclarConteudos = (conteudos) => conteudos.join(" ");
+const separarPorLinhas = (todoConteudo) => todoConteudo.split("\n");
+const separarPorPalavras = (todoConteudo) => todoConteudo.split(" ");
+
 fn.lerDiretorio(caminhoPasta)
     .then((caminhos) => fn.filtrarExtensao(caminhos, ".srt"))
     .then((listaCaminhos) => fn.lerArquivos(listaCaminhos))
-    .then((conteudos) => conteudos.join("\n"))
-    .then((todoConteudo) => todoConteudo.split("\n"))
+    .then(mesclarConteudos)
+    .then(separarPorLinhas)
     .then(fn.removerSeVazio)
     .then((linhas) => fn.removerSeIncluir("-->", linhas))
     .then(fn.removerSeApenasNumero)
     .then((linhas) => fn.removerSimbolos(simbolos, linhas))
+    .then(mesclarConteudos)
+    .then(separarPorPalavras)
     .then(console.log);
