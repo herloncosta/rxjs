01532f3cc34dadd561105d954a70dde65f35a8bb
herloncosta <herlon36@gmail.com>
2022-07-31 19:01:13 -0300

    funcao pura 2

---
+++ paradigma-funcional/funcao_pura_2.js
@@ -0,0 +1,13 @@
+// Impura - são gerados valores aleatórios com
+// dependência de uma biblioteca externa
+
+function gerarNumeroAleatorio(min, max) {
+    const fator = max - min + 1;
+    return parseInt(Math.random() * fator) + min;
+}
+
+console.log(gerarNumeroAleatorio(1, 10000));
+console.log(gerarNumeroAleatorio(1, 10000));
+console.log(gerarNumeroAleatorio(1, 10000));
+console.log(gerarNumeroAleatorio(1, 10000));
+console.log(gerarNumeroAleatorio(1, 10000));
