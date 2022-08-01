f5088e87a035befd74e143b000f9083fe34ae0be
herloncosta <herlon36@gmail.com>
2022-08-01 19:08:15 -0300

    refactoring imutabilidade 2

--- paradigma-funcional/imutabilidade_2.js
+++ paradigma-funcional/imutabilidade_2.js
@@ -14,10 +14,9 @@ const nums = [4, 8, 3, 2, 9, 1, 9, 3];
 
 // 3 - Recursividade
 function somarArray(array, total = 0) {
-    if (array.length === 0) {
-        return total;
-    }
-    return somarArray(array.slice(1), total + array[0]);
+    return array.length === 0
+        ? total
+        : somarArray(array.slice(1), total + array[0]);
 }
 
 const total = somarArray(nums);
