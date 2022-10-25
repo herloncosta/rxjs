d13b4336fe4b96161e07d2fc523b064d2b3801de
herloncosta <herlon36@gmail.com>
2022-10-25 19:14:23 -0300

    implementando interval rxjs

--- rxjs/stream_dados3.js
+++ rxjs/stream_dados3.js
@@ -1,1 +1,9 @@
-import rxjs from "rxjs";
+import { interval } from "rxjs";
+
+const gerarNumeros = interval(500);
+
+const sub1 = gerarNumeros.subscribe((num) => console.log(Math.pow(num, 2)));
+const sub2 = gerarNumeros.subscribe(console.log);
+
+setTimeout(() => sub1.unsubscribe(), 8000);
+setTimeout(() => sub2.unsubscribe(), 6000);
