46bfe700f157e89c0b2097aaa5385c364e6364cf
herloncosta <herlon36@gmail.com>
2022-10-27 19:36:58 -0300

    implementando scheduler

--- rxjs/scheduler.js
+++ rxjs/scheduler.js
@@ -3,7 +3,7 @@ import { from, asyncScheduler, observeOn } from "rxjs";
 console.log("Antes...");
 
 from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-	// .pipe(observeOn(asyncScheduler))
+	.pipe(observeOn(asyncScheduler))
 	.subscribe(console.log);
 
 console.log("Depois...");
